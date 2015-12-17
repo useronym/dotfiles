decd7ef2d6a07693cc16359d1f752a0a51235ca3
osense <krupicka.adam@gmail.com>
2015-12-17 09:42:59 +0100

    ...

--- bin/panel.sh
+++ bin/panel.sh
@@ -52,7 +52,7 @@ Weather() {
     case $WEATHER in
         *fog* | *mist*) echo -ne '%{T2}\uf070%{T1}';;
         *storm*) echo -ne '%{T2}\uf0e7%{T1}';;
-        *rain*) echo -ne '%{T2}\uf043%{T1}';;
+        *rain* | *drizzle*) echo -ne '%{T2}\uf043%{T1}';;
         *cloud*) echo -ne '%{T2}\uf0c2%{T1}';;
         *snow*) echo -ne '%{T2}\uf069%{T1}';;
         *sun* | *clear*)
