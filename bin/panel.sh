9a7c2db900d5ef4a60b61d7e7b860dd1114d891d
osense <krupicka.adam@gmail.com>
2016-02-22 22:21:23 +0100

    ...

--- bin/panel.sh
+++ bin/panel.sh
@@ -48,10 +48,11 @@ Backlight() {
 }
 
 Weather() {
-    URL='http://www.accuweather.com/en/cz/brno/123291/weather-forecast/123291'
-    WEATHER=$(wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2 " "  $14", "$12"°" }'| head -1)
+    DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=3078610&appid=1b3106852d5d55db8af8bdc5ccd2313f')
+    WEATHER=$(echo $DATA | jq -r '.weather[0].main')
+    TEMP=$(echo $DATA | jq -r '.main.temp')
+    TEMP=${TEMP%.*}
     NIGHT=$(echo $WEATHER | cut -d " " -f1)
-    WEATHER=$(echo $WEATHER | cut -d " " -f1 --complement)
     shopt -s nocasematch
     case $WEATHER in
         *fog* | *mist*) echo -ne '%{T2}\uf070%{T1}';;
@@ -66,7 +67,7 @@ Weather() {
                 echo -ne '%{T2}\uf185%{T1}'
             fi;;
     esac
-    echo -n " $WEATHER"
+    echo -n " $WEATHER, $(($TEMP-273))°C"
 }
 
 Mail() {
