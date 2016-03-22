bba47add0441759739f29ef8f781fd7a1f282a5f
osense <krupicka.adam@gmail.com>
2016-03-22 22:45:44 +0100

    ...

--- bin/panel.sh
+++ bin/panel.sh
@@ -49,6 +49,8 @@ Backlight() {
 
 Weather() {
     DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=3078610&appid=1b3106852d5d55db8af8bdc5ccd2313f')
+    if [ "$?" -ne "0" ]; then return -1; fi
+
     WEATHER=$(echo $DATA | jq -r '.weather[0].main')
     TEMP=$(echo $DATA | jq -r '.main.temp')
     TEMP=${TEMP%.*}
