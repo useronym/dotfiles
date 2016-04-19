e136ee4352edfb9891f73965a5cc73c5c59a6cd0
Adam Krupicka <akr@mail.muni.cz>
2016-04-19 16:51:57 +0200

    ...

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -2,22 +2,30 @@
 
 calc() { awk "BEGIN{printf \"%2.0f\", $*}"; }
 
-
-WALLPS=(~/Pictures/*)
+WALLPS=(~/Pictures/*.{jpg,png})
 SND_MON="$HOME/Pictures/Dark-Black-Background.jpg"
 
 rnd=$(( $(date +%s) % ${#WALLPS[@]} ))
-killall -q panel.sh
+wallp=${WALLPS[rnd]}
+wallp_file="$wallp.color"
 
-data=$(convert ${WALLPS[rnd]} -crop x50 -colorspace gray -verbose info:)
-mean=$(echo "$data" | sed -n '/^.*[Mm]ean:.*[(]\([0-9.]*\).*$/{ s//\1/; p; q; }')
-if [ $(calc "$mean * 100") -ge 66 ]; then
-    panel_fg="#FF000000"
+mkdir -p $DATA_DIR
+if [ -f $wallp_file ]; then
+    read -r line < $wallp_file
+    $panel_fg = "$line"
 else
-    panel_fg="#FFFFFFFF"
+    data=$(convert $wallp -crop x50 -colorspace gray -verbose info:)
+    mean=$(echo "$data" | sed -n '/^.*[Mm]ean:.*[(]\([0-9.]*\).*$/{ s//\1/; p; q; }')
+    if [ $(calc "$mean * 100") -ge 66 ]; then
+        panel_fg="#FF000000"
+    else
+        panel_fg="#FFFFFFFF"
+    fi
+    echo $panel_fg > $wallp_file
 fi
 
 
+killall -q panel.sh
 $XDG_CONFIG_HOME/bspwm/bspwmrc
 feh --bg-fill ${WALLPS[rnd]} --bg-fill $SND_MON
 panel.sh | lemonbar -fMonospace:size=10 -fFontAwesome:size=12 -F$panel_fg -B"#00000000" -g1600x25 eDP1 | zsh &
