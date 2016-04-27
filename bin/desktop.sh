ec50822eaf650b34be2d0a078187bad7d3059f0d
Adam Krupicka <akr@mail.muni.cz>
2016-04-27 10:12:43 +0200

    …

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -9,10 +9,9 @@ rnd=$(( $(date +%s) % ${#WALLPS[@]} ))
 wallp=${WALLPS[rnd]}
 wallp_file="$wallp.color"
 
-mkdir -p $DATA_DIR
 if [ -f $wallp_file ]; then
     read -r line < $wallp_file
-    $panel_fg = "$line"
+    panel_fg="$line"
 else
     data=$(convert $wallp -crop x50 -colorspace gray -verbose info:)
     mean=$(echo "$data" | sed -n '/^.*[Mm]ean:.*[(]\([0-9.]*\).*$/{ s//\1/; p; q; }')
@@ -28,4 +27,4 @@ fi
 killall -q panel.sh
 $XDG_CONFIG_HOME/bspwm/bspwmrc
 feh --bg-fill ${WALLPS[rnd]} --bg-fill $SND_MON
-panel.sh | lemonbar -fMonospace:size=10 -fFontAwesome:size=12 -F$panel_fg -B"#00000000" -g1600x25 eDP1 | zsh &
+panel.sh | lemonbar -f'DejaVu Sans Mono':size=10 -fFontAwesome:size=12 -F$panel_fg -B"#00000000" -g1600x25 eDP1 | zsh &
