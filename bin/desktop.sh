27279c966c7789c740653df2d7b78d4247251e4f
Adam Krupicka <akr@mail.muni.cz>
2016-10-16 10:53:45 +0200

    …

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -2,7 +2,12 @@
 
 calc() { awk "BEGIN{printf \"%2.0f\", $*}"; }
 
-WALLPS=(~/Pictures/*.{jpg,png})
+if [ $(sunwait poll 49.214238N 16.601385E) = "DAY" ]; then
+    WALLPS=(~/Pictures/day/*.{jpg,png})
+else
+    WALLPS=(~/Pictures/night/*.{jpg,png})
+fi
+PANEL_MON="eDP1"
 SND_MON="$HOME/Pictures/Dark-Black-Background.jpg"
 
 rnd=$(( $(date +%s) % ${#WALLPS[@]} ))
@@ -15,7 +20,7 @@ if [ -f $wallp_file ]; then
 else
     data=$(convert $wallp -crop x50 -colorspace gray -verbose info:)
     mean=$(echo "$data" | sed -n '/^.*[Mm]ean:.*[(]\([0-9.]*\).*$/{ s//\1/; p; q; }')
-    if [ $(calc "$mean * 100") -ge 66 ]; then
+    if [ $(calc "$mean * 100") -ge 67 ]; then
         panel_fg="#FF000000"
     else
         panel_fg="#FFFFFFFF"
@@ -27,4 +32,5 @@ fi
 killall -q panel.sh
 $XDG_CONFIG_HOME/bspwm/bspwmrc
 feh --bg-fill ${WALLPS[rnd]} --bg-fill $SND_MON
-panel.sh | lemonbar -f'DejaVu Sans Mono':size=10 -fFontAwesome:size=12 -F$panel_fg -B"#00000000" -g1600x25 eDP1 | zsh &
+RESX=$(xrandr --current | grep $PANEL_MON | cut -d ' ' -f4 | cut -d 'x' -f1)
+panel.sh | lemonbar -f'DejaVu Sans Mono':size=10 -fFontAwesome:size=12 -F$panel_fg -B"#00000000" -g"$RESX"x25 $PANEL_MON | zsh &
