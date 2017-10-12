bab1bbc1dcdcf4a431e804c51b592ae60e5ee45d
Adam Krupicka <akr@mail.muni.cz>
2017-10-12 20:47:42 +0200

    \.

--- bin/bar.sh
+++ bin/bar.sh
@@ -5,5 +5,5 @@ killall -q lemonbar
 source "colors.sh"
 PANEL_MON="eDP1"
 
-admiral | lemonbar -f'Misc Ohsnap':size=14 -f'Wuncon Siji':size=13 -F"$c_fg" -B"$c_bg" -U"$c_fg" -gx25 $PANEL_MON | zsh
+admiral | lemonbar -f'Misc Ohsnap':size=14 -f'Wuncon Siji':size=13 -F"$c_fg" -B"$c_bg" -U"$c_fg" -gx20 $PANEL_MON | zsh
 
---
+++ schemes/shroom/colors.sh
@@ -0,0 +1,4 @@
+c_fg='#FFFFFF'
+c_bg='#000000FF'
+c_red='#E92f2f'
+c_blue='#3B48E3'
