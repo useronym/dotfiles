68d4be15a5c0404c4e8fc88c0bf3e9c47ea1cea7
Adam Krupicka <akr@mail.muni.cz>
2018-04-02 11:19:48 +0200

    …

--- .config/admiral.d/mail.sh
+++ .config/admiral.d/mail.sh
@@ -5,5 +5,5 @@ source "colors.sh"
 MAIL=$(mailcheck -c)
 if [ "$MAIL" != "" ]; then
     MAILS=$(echo $MAIL | cut -d " " -f 3)
-    echo -ne "%{B$c_bg} %{T2}\ue1a8%{T1}%{B-} $MAILS new "
+    echo -ne "%{R} %{T2}\ue1a8%{T1}%{R} $MAILS new "
 fi
--- .config/admiral.d/weather.sh
+++ .config/admiral.d/weather.sh
@@ -2,8 +2,8 @@
 
 source "colors.sh"
 
-DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=723846&appid=1b3106852d5d55db8af8bdc5ccd2313f')
-#DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=3078610&appid=1b3106852d5d55db8af8bdc5ccd2313f')
+#DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=723846&appid=1b3106852d5d55db8af8bdc5ccd2313f')
+DATA=$(curl 'http://api.openweathermap.org/data/2.5/weather?id=3078610&appid=1b3106852d5d55db8af8bdc5ccd2313f')
 if [ "$?" -ne "0" ]; then exit -1; fi
 
 
