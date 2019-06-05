44c0ba7278ffebc265b2a01e030e82c8eb84d855
Adam Krupicka <akr@mail.muni.cz>
2019-06-05 23:18:50 +0200

    …

--- bin/bar2.sh
+++ bin/bar2.sh
@@ -4,10 +4,5 @@ sleep 1 # so that bar.sh doesn't kill us
 
 source "colors.sh"
 
-PANEL_MON=$(get_snd_mon.sh)
-if [[ "$PANEL_MON" == "" ]]; then
-    exit 0
-fi
-
-admiral -c ~/.config/admiral.d/secondary.toml | lemonbar -b -f'Misc Ohsnap':size=14 -f'Siji':size=13  -F$c_fg -B$c_bg -U$c_fg -gx25 $PANEL_MON | zsh
+admiral -c ~/.config/admiral.d/secondary.toml | lemonbar -f'Misc Ohsnap':size=14 -f'Siji':size=13  -F$c_fg -B$c_bg -U$c_fg -gx20 default | zsh
 
